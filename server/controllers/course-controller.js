75ee6af40249d4fb9b69f2e357c80c052bba110b
erinvarey <vareye@mcmaster.ca>
2017-03-18 00:41:23 -0400

    adding methods i think

--- server/controllers/course-controller.js
+++ server/controllers/course-controller.js
@@ -4,8 +4,24 @@ import * as firebase from 'firebase';
 export default {
     createCourse(req, res) {
         let course = req.body;
-        firebase.database().ref().child('courses').push(course).then(function(snapshot){
+        firebase.database().ref().child('course').push(course).then(function(snapshot){
             Response.sendMessage(res, 'Successfully created course');
+        });
+        firebase.database().ref().child('course').get(course).then(function(snapshot){
+        	Response.sendMessage(res, 'Successfully Grabbed course');
+        });
+        firebase.database().ref().child('course').put(course).then(function(snapshot){
+        	Response.sendMessage(res, 'Successfully create course');
+        });
+        firebase.database().ref().child('course').post(course).then(function(snapshot){
+        	Response.sendMessage(res, 'Successfully updated course');
+        });
+        firebase.database().ref().child('course').delete(course).then(function(snapshot){
+        	Response.sendMessage(res, 'Successfully removed course');
         });
     }
 };
+//get grab
+//put create
+//post update
+//delete
